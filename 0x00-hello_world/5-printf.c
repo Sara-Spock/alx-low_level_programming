62ffef9b24fdda15d62e4b112b967481d32dc7b1
Sara Abdi Hersi <hersae.sara21@gmail.com>
2023-02-16 14:04:14 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-i
+#include <stdio.h>
+/**
+ * main -a program that prints line with printf function
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
