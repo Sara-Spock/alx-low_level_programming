1ae495ed7950bc781a0d2fe07750cce98be90d5e
Sara Abdi Hersi <hersae.sara21@gmail.com>
2023-02-16 13:59:16 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - a program that prints a line in puts function
- * Return 0 (Success)
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
  */
 int main(void)
 {
