211e64c0e0613b4c83b697a341905e0664cef093
Sara Abdi Hersi <hersae.sara21@gmail.com>
2023-02-17 14:54:38 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,11 +14,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
+	{
 		printf("%lu is positive\n", n);
+	}
 	else if (n < 0)
+	{
 		printf("%lu is negative\n", n);
+	}
 	else
+	{
 		printf("%lu is zero\n", n);
-
+	}
 	return (0);
 }
