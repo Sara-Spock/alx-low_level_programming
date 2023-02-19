c9d778062d5227c3406bc34b3dd692666cc2433f
Sara Abdi Hersi <hersae.sara21@gmail.com>
2023-02-19 15:15:21 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints all possible different combinations of three digits
+ * Return Always 0 (success)
+ */
+int main(void)
+{
+	int m, d, s;
+
+	for (m = '0'; m <= '9'; m++)
+	{
+		for (d = '0'; d <= '9'; d++)
+		{
+			for (s = '0'; s <= '9'; s++)
+			{
+				if (m < d && d < s)
+				{
+					putchar(m);
+					putchar(d);
+					putchar(s);
+
+					if (m != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	puthcar('\n');
+	return (0);
+}
