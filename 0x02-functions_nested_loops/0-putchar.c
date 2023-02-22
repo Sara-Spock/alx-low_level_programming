4223108a4779a1c2896b4f2e16bae2e2d41fb884
Sara Abdi Hersi <hersae.sara21@gmail.com>
2023-02-22 14:19:17 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,21 @@
+#include "main.h"
 
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char str[] = "_putchar\n";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		char c = str[i];
+		
+		_putchar(c);
+		i++;
+	}
+	return (0);
+}
