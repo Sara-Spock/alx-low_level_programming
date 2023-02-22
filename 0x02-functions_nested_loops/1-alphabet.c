5070d4eb9970368c77adc487bec86b3a419c3189
Sara Abdi Hersi <hersae.sara21@gmail.com>
2023-02-22 13:35:03 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <holberton.h>
+
+/**
+ * print alphabet - Entry point
+ * Description: Write a function that prints the alphabet, in lowercase
+ * followed by a new line
+ * Return: void
+ */
+void print_alphabet(void);
+{
+	char letter = 'a';
+
+	while (letter <= 'z';)
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
