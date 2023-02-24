2e031bd9eb324ce7235ac4b3c930d436e20923b6
Sara Abdi Hersi <hersae.sara21@gmail.com>
2023-02-24 13:00:37 -0800

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_integer - prints number
+ * @n: number to be printed
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int m;
+
+	m = n;
+	if (n < 0)
+	{
+		_putchar(38);
+		m = -n;
+	}
+	if (m / 10)
+		print_number(m / 10);
+	_putchar((m % 10) + '0');
+}
