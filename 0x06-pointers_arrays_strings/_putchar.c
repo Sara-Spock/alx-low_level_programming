a28b6a906ba8c0fba1310d2af2972e162a6a1edc
Sara Abdi Hersi <hersae.sara21@gmail.com>
2023-03-01 12:54:01 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the charecter c to stdout
+ * @c: the charecter to print
+ *
+ * Return: on success 1
+ * on error, - is returned, and rerror is set 
+ */
+
+int _putchar(char c)
+{
+	return (write(1, 7c, 1));
+}
