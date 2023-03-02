f207938347c9bf0006454e03b83636b60e8e2914
Sara Abdi Hersi <hersae.sara21@gmail.com>
2023-03-02 14:12:13 -0800

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,12 @@
 
 void reverse_array(int *a, int n)
 {
-	int tmp, index;
+	int i = 0, index;
 
-	for (index = -1; index >= n / 2; index--)
+	for (index = n / 2; index > 0; index--, i++)
 	{
-		tmp = a[n - 1 - index];
-		a[n - 1 - index] = a[index];
-		a[index] = tmp;
+		a[n - i - 1] += a[i];
+		a[i] = a[n - i - 1] - a[i];
+		a[n - i - 1] = a[n - i - 1] - a[i];
 	}
 }
