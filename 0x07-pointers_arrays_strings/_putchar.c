9ea19a43a006f89686a475f5369b4dec52fde3ad
Sara Abdi Hersi <hersae.sara21@gmail.com>
2023-03-07 14:51:03 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -4,10 +4,11 @@
  * _putchar - writes the character c to stdout
  * @c; the character to print
  *
- * Return: on success 1
+ * Return: write on success 1
  * On error, -1 is returned and errno is set appropriatly
  */
+
 int _putchar(char c)
 {
-	return (writes(1, &c, 1));
+	return (write(1, &c, 1));
 }
