1997d1a3aa7a9808e800a654d2ea03eddd5dec35
Sara Abdi Hersi <hersae.sara21@gmail.com>
2023-03-09 15:39:35 -0800

    6-is_prime-number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -13,10 +13,8 @@ int is_prime_number(int n);
  */
 int is_divisible(int num, int div)
 {
-	int num, int div
-		
-		if (num % div == 0)
-			return (0);
+	if (num % div == 0)
+		return (0);
 	
 	if (div == num / 2)
                 return (1);
